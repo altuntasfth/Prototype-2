33d801e5e918be8f80163af8d7afe53dd7de6ea8
Fatih Altuntas <fatihaltuntas44@gmail.com>
2020-05-01 13:28:57 +0300

    Add unity project

---
+++ Assets/Scrpits/MoveForward.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float verticalInput;
+    public float speed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
+        }
+        
+        
+    }
+}
